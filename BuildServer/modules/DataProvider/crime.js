92541465afbf2cb0e5824e9bbcb8f081c271561c
christianvazquez7 <christian.vazquez7@upr.edu>
2015-10-06 15:14:51 -0400

    Crime parsing and modeling done

--- BuildServer/modules/DataProvider/crime.js
+++ BuildServer/modules/DataProvider/crime.js
@@ -1,15 +1,15 @@
 /**
  * POJO object for structured crime data.
  */
-module.exports = function Crime(){
+module.exports = function Crime(pLatitude,pLongitude,pType,pDate,pTime,pId,pIsDomestic){
 	
-	var latitude;
-	var longitude;
-	var type;
-	var date;
-	var time;
-	var id;
-	var isDomestic;
+	var latitude = pLatitude;
+	var longitude = pLongitude;
+	var type = pType;
+	var date = pDate;
+	var time = pTime;
+	var id = pId;
+	var isDomestic = pIsDomestic;
 
 	/**
 	 * Parses a Json list into a structured list of Crime data.
@@ -18,55 +18,68 @@ module.exports = function Crime(){
 	 * @param marshall: Translator of api labels.
 	 */
 	Crime.fromList = function(jsonList, marshall) {
+		var crimeList = [];
+		for (var i =0 ; i < jsonList.length ; i++) {
+			var crimeJson = jsonList[i];
+			var latitude = crimeJson[marshall.getLatitudeLabel()];
+			var longitude = crimeJson[marshall.getLongitudeLabel()];
+			var type = crimeJson[marshall.getTypeLabel()];
+			var date = crimeJson[marshall.getDateLabel()];
+			var time = crimeJson[marshall.getTimeLabel()];
+			var id = crimeJson[marshall.getIdLabel()];
+			var isDomestic = crimeJson[marshall.getDomesticLabel()];
 
+			crimeList.push(new Crime(latitude,longitude,type,date,time,id,isDomestic));
+		}
+		return crimeList;
 	};
 
 	/**
 	 * Gets the latitude of the crime.
 	 */
 	this.getLatitude = function() {
-
+		return latitude;
 	};
 
 	/**
 	 * Gets the longitude of the crime.
 	 */
 	this.getLongitude = function() {
-
+		return longitude;
 	};
 
 	/**
 	 * Gets the type of the crime.
 	 */
 	this.getType = function() {
-
+		return type;
 	};
 
 	/**
 	 * Gets the date of the crime.
 	 */
 	this.getDate = function() {
-
+		return date;
 	};
 
 	/**
 	 * Gets the time of the crime.
 	 */
 	this.getTime = function() {
-
+		return time;
 	};
 
 	/**
 	 * Gets the Id of the crime.
 	 */
 	this.getId = function() {
-
+		return id;
 	};
 
 	/**
 	 * Gets the domestic attribute of the crime.
 	 */
 	this.isDomestic = function() {
-
+		return isDomestic;
 	};
 };
