3bb80fcf11c92108ddd62801022b4a3db664b33a
christianvazquez7 <christian.vazquez7@upr.edu>
2015-10-12 11:11:11 -0400

    Adding all skeletons and xml files to the Android studio project

--- Android/app/src/main/java/com/nvbyte/kya/MainActivity.java
+++ Android/app/src/main/java/com/nvbyte/kya/MainActivity.java
@@ -2,23 +2,38 @@ package com.nvbyte.kya;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
+
+/**
+ * Activity that is triggered by user opening app from the menu. In it, the user can see the current
+ * area of risk and modify settings for the KYA application.
+ */
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
+
+    private ViewPager mMainContentPager;
+    private FragmentPagerAdapter mMainContentAdapter;
+    private CurrentZoneFragment mCurrentZoneFragment;
+    private SettingsFragment mSettingsFragment;
+
 
     @Override
+    /**
+     * Creates fragments if they do not exists and adds them to the ViewPager.
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+
+    }
+
+    @Override
+    /**
+     * Calls refresh on the current zone fragment to fetch up to date information about location.
+     */
+    protected void onResume() {
+        super.onResume();
     }
 }
