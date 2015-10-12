5e35f3355050a11a0441af58ec503c5d016cebfe
christianvazquez7 <christian.vazquez7@upr.edu>
2015-10-12 12:18:59 -0400

    Threshold option fragment implemented(untested).

--- Android/app/src/main/java/com/nvbyte/kya/ThresholdOptionFragment.java
+++ Android/app/src/main/java/com/nvbyte/kya/ThresholdOptionFragment.java
@@ -1,7 +1,9 @@
 package com.nvbyte.kya;
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,11 +17,28 @@ import android.widget.TextView;
 public class ThresholdOptionFragment extends Fragment {
 
     private TextView mThresholdTextView;
+    private TextView mSelectedView;
+    private static final String THRESHOLD = "THRESHOLD";
+    private static final String SELECTED = "SELECTED";
+    private int mThreshold;
+    private boolean mIsSelected;
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mThreshold = getArguments().getInt(THRESHOLD);
+        mIsSelected = getArguments().getBoolean(SELECTED);
+    }
 
     //Creates a new threshold fragment for a particular threshold option.
-    public static ThresholdOptionFragment forThreshold(int threshold) {
-        return null;
+    public static ThresholdOptionFragment forThreshold(int threshold, boolean isSelected) {
+        ThresholdOptionFragment fragment = new ThresholdOptionFragment();
+        Bundle args = new Bundle();
+        args.putInt(THRESHOLD,threshold);
+        args.putBoolean(SELECTED,isSelected);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Nullable
@@ -28,7 +47,16 @@ public class ThresholdOptionFragment extends Fragment {
      * Creates the view for the threshold option.
      */
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_threshold, container, false);
+        mThresholdTextView = (TextView) rootView.findViewById(R.id.threshold);
+        mSelectedView = (TextView) rootView.findViewById(R.id.isSelected);
+        mThresholdTextView.setText(Integer.toString(mThreshold));
+        if(mIsSelected) {
+            mSelectedView.setVisibility(View.VISIBLE);
+        } else {
+            mSelectedView.setVisibility(View.INVISIBLE);
+        }
+        return rootView;
     }
 
     /**
@@ -37,7 +65,7 @@ public class ThresholdOptionFragment extends Fragment {
      * @return A valuew between 1-9, where 1 is the lowest threshold and 9 is the highest threshold.
      */
     public int getThreshold() {
-        return 0;
+        return mThreshold;
     }
 
 }
