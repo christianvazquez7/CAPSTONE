880d06389323965ba6bf2c4f66ab4f49a34743bf
christianvazquez7 <christian.vazquez7@upr.edu>
2015-10-14 17:33:47 -0400

    Request Manager implemented (untested)

---
+++ Android/app/src/main/java/com/nvbyte/kya/ByteArrayRequest.java
@@ -0,0 +1,40 @@
+package com.nvbyte.kya;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+
+import org.apache.http.HttpStatus;
+
+public class ByteArrayRequest extends Request<byte[]> {
+    private final Response.Listener<byte[]> mListener;
+    private final byte[] mBody;
+    public ByteArrayRequest(int method, String url, byte[] body, Response.Listener<byte[]> listener, Response.ErrorListener errorListener) {
+        super(method, url, errorListener);
+        mBody = body;
+        mListener = listener;
+    }
+    @Override
+    protected Response<byte[]> parseNetworkResponse(NetworkResponse response) {
+        if(response == null){
+            return null;
+        }
+        if(response.statusCode != HttpStatus.SC_OK){
+            return null;
+        }
+        byte[] bytes = response.data;
+        return Response.success(bytes, null);
+    }
+    @Override
+    protected void deliverResponse(byte[] response) {
+        if(mListener != null){
+            mListener.onResponse(response);
+        }
+    }
+
+    @Override
+    public byte[] getBody() throws AuthFailureError {
+        return mBody;
+    }
+}
--- Android/app/src/main/java/com/nvbyte/kya/RequestManager.java
+++ Android/app/src/main/java/com/nvbyte/kya/RequestManager.java
@@ -1,8 +1,14 @@
 package com.nvbyte.kya;
 
 
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
 
 /**
  * Singleton that manages all HTTP requests to remote service using Volley API.
@@ -10,16 +16,19 @@ import com.android.volley.Response;
 public class RequestManager {
     private RequestQueue mRequestQueue;
     private static RequestManager mRequestManager;
-    private static final String SURVEY_ROUTE = "";
-    private static final String HEART_BEAT_ROUTE = "";
-    private static final String CHECK_IN_ROUTE = "";
-    private static final String CURRENT_ZONE_ROUTE = "";
+    private static final String SURVEY_ROUTE = "www.projectkya.com/survey";
+    private static final String HEART_BEAT_ROUTE = "www.projectkya.com/heartbeat";
+    private static final String CHECK_IN_ROUTE = "www.projectkya.com/checkin";
+    private static final String CURRENT_ZONE_ROUTE = "www.projectkya.com/current";
+    private Context mContext;
+    private static final String TAG = "RequestManager";
 
     /**
      * Private constructor invoked by static factory to avoid multiple instantiations.
      */
-    private RequestManager() {
-
+    private RequestManager(Context context) {
+        mContext = context;
+        mRequestQueue = Volley.newRequestQueue(mContext);
     }
 
     /**
@@ -28,8 +37,11 @@ public class RequestManager {
      *
      * @return Request Manager singleton, that manages all HTTP requests.
      */
-    public static RequestManager getInstance() {
-        return null;
+    public static RequestManager getInstance(Context context) {
+        if(mRequestManager == null) {
+            mRequestManager = new RequestManager(context);
+        }
+        return mRequestManager;
     }
 
     /**
@@ -38,8 +50,20 @@ public class RequestManager {
      * @param rating   Rating selected by the user in response to a survey.
      * @param deviceId Unique identifier for wearer device.
      */
-    public void sendSurveyResponse(long deviceId, int rating) {
-
+    public void sendSurveyResponse(long deviceId,long notificationId, int rating) {
+        byte[] body = null;
+        ByteArrayRequest request = new ByteArrayRequest(Request.Method.POST,SURVEY_ROUTE,body,new Response.Listener<byte[]>() {
+            @Override
+            public void onResponse(byte[] response) {
+                Log.d(TAG,"Got response for survey");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG,"Error submitting survey to server.");
+            };
+        });
+        mRequestQueue.add(request);
     }
 
     /**
@@ -48,8 +72,20 @@ public class RequestManager {
      * @param bmp      Heart beat readings in beats per minute.
      * @param deviceId Unique identifier for wearer device.
      */
-    public void sendHeartBeatReading(long deviceId, int bmp) {
-
+    public void sendHeartBeatReading(long deviceId, long notificationId , int bmp) {
+        byte[] body = null;
+        ByteArrayRequest request = new ByteArrayRequest(Request.Method.POST,HEART_BEAT_ROUTE,body,new Response.Listener<byte[]>() {
+            @Override
+            public void onResponse(byte[] response) {
+                Log.d(TAG,"Got response for heartbeat");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG,"Error submitting heartbeat to server.");
+            };
+        });
+        mRequestQueue.add(request);
     }
 
     /**
@@ -62,8 +98,10 @@ public class RequestManager {
      * @param onSuccess Success callback, called when request is serviced.
      * @param onFailure Error callback, called when a request does not fall through.
      */
-    public void checkIn(long deviceId, int velocity, double lat, double lon, Response.Listener<Byte[]> onSuccess, Response.ErrorListener onFailure) {
-
+    public void checkIn(long deviceId, int velocity, double lat, double lon, Response.Listener<byte[]> onSuccess, Response.ErrorListener onFailure) {
+        byte[] body = null;
+        ByteArrayRequest request = new ByteArrayRequest(Request.Method.POST,CHECK_IN_ROUTE,body,onSuccess, onFailure);
+        mRequestQueue.add(request);
     }
 
     /**
@@ -74,15 +112,17 @@ public class RequestManager {
      * @param onSuccess Success callback, called when request is serviced.
      * @param onFailure Error callback, called when a request does not fall through.
      */
-    public void getCurrentZone(double lat, double lon, Response.Listener<Byte[]> onSuccess, Response.ErrorListener onFailure) {
-
+    public void getCurrentZone(double lat, double lon, Response.Listener<byte[]> onSuccess, Response.ErrorListener onFailure) {
+        byte[] body = null;
+        ByteArrayRequest request = new ByteArrayRequest(Request.Method.POST,CURRENT_ZONE_ROUTE,body,onSuccess, onFailure);
+        mRequestQueue.add(request);
     }
 
     /**
      * Cancel all pending HTTP requests.
      */
-    public void cancelPendingRequests() {
-
+    public void cancelPendingRequests(Object tag) {
+        mRequestQueue.cancelAll(tag);
     }
 
 
